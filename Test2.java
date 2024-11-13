3fd8c2db410871ca2d9c011252b784c7d8e21568
double2D1812 <zingme6677028@gmail.com>
2024-11-13 20:41:46 +0700

    Delete Test2.java

--- Test2.java
+++
@@ -1,8 +0,0 @@
-package core2;
-
-public class Test2 {
-	public static void main(String[] args) {
-		DiaChi p1 = new  DiaChi("ThoSon", "ThiTranDoanHung", "DoanHung", "PhuTho");
-		System.out.println("Thong tin cua ban la "+p1.toString());
-	}
-}
